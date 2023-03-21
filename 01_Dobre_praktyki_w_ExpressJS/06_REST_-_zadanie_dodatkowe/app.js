0aaa5a90faf547fc25ec91423e0347d95fd3ea28
orzeelekk <orzellpawell@gmail.com>
2023-03-21 11:13:44 +0100

    wyk zad 02

--- 01_Dobre_praktyki_w_ExpressJS/06_REST_-_zadanie_dodatkowe/app.js
+++ 01_Dobre_praktyki_w_ExpressJS/06_REST_-_zadanie_dodatkowe/app.js
@@ -3,10 +3,26 @@ import rateLimit from 'express-rate-limit'
 import compression from "compression"
 import morgan from 'morgan'
 const app = express();
+
 const user = {
     name: "Pan Pawel",
     modified: new Date()
 }
+
+//ten app set i app get sa powiazane i musza byc wypisane aby skorzystac z warunkowego wysylania danych
+app.set('etag','strong')
+app.get('/user', (req,res) => {
+        res.append('Last-Modified',user.modified.toString())
+        res.json(user)
+    }
+)
+app.put('/user/:name',(req,res) => {
+    user.name = req.params.name
+    user.modified = new Date()
+    res.send('Nowe name i date')
+})
+
+
 const limiter = rateLimit({
     windowMs:120000,
     max: 4,
@@ -25,18 +41,8 @@ app.get('/test/:tekst',(req,res) => {
     const reverse = req.params.tekst.split('').reverse().join('')
     res.send(reverse.repeat(1000))
 })
-//ten app set i app get sa powiazane.
-app.set('etag','strong')
-app.get('/user', (req,res) => {
-    res.append('Last-Modified',user.modified.toString())
-    res.json(user)
-    }
-)
-app.put('/user/:name',(req,res) => {
-    user.name = req.params.name
-    user.modified = new Date()
-    res.send('Nowe name i date')
-})
+
+
 app.listen(5000,() => {
     console.log("Listening on 5000")
 })
