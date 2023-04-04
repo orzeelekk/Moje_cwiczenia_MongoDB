94dd3807b97651f7c416dc3cef38042ac8f0a19c
orzeelekk <orzellpawell@gmail.com>
2023-04-04 13:23:38 +0200

    wyk zad 02

--- 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/02_Wlasciwosci_wirtualne/task.js
+++ 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/02_Wlasciwosci_wirtualne/task.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-import { connectToMongoose } from './internals/connect';
-import { runAssertions } from './internals/assertions';
+import { connectToMongoose } from './internals/connect.js';
+import { runAssertions } from './internals/assertions.js';
 
 (async function () {
   try {
@@ -11,6 +11,80 @@ import { runAssertions } from './internals/assertions';
     let Book; // Book model should be assigned to this property
 
     // Add all of your code below
+    const booksSchema = new mongoose.Schema({
+      author: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      language: {
+        type: String,
+        required: true,
+      },
+      pages: {
+        type: Number,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      year: {
+        type: Number,
+        required: true
+      },
+      amount: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      }
+    });
+
+    booksSchema.virtual('totalStoreValue')
+        .get(function () {
+          return this.amount * this.price
+        })
+    booksSchema.virtual('bookDetails')
+        .set(function(detailsString) {
+          const splitParts = detailsString.split(" - ");
+          this.author = splitParts[0]
+          this.title = splitParts[1]
+        })
+        .get(function () {
+          return `${this.author} - ${this.title}`
+        })
+
+    Book = mongoose.model('Book',booksSchema)
+
+    books = await Book.findOne()
+        .where('amount')
+        .gte(40)
+        .exec()
+
+    console.log(books)
+    console.log(books.totalStoreValue)
+
+    const book = new Book({
+          "author": "Gabriel Garc\u00eda M\u00e1rquez",
+          "country": "Colombia",
+          "language": "Spanish",
+          "pages": 417,
+          "title": "One Hundred Years of Solitude",
+          "year": 1967,
+          "amount": 20,
+          "price": 42.5
+        }
+    )
+
+    book.bookDetails = "Gabriel Garc\u00eda M\u00e1rquez - One Hundred Years of Solitude"
+    await book.save()
+    console.log(book)
 
     await runAssertions(Book, books);
   } catch (err) {
