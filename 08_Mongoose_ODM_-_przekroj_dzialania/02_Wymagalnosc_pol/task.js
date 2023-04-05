dd076fcdfabff4069dc48076c3ec72fcfc1ebc67
orzeelekk <orzellpawell@gmail.com>
2023-04-05 13:33:44 +0200

    wyk zad 02

--- 08_Mongoose_ODM_-_przekroj_dzialania/02_Wymagalnosc_pol/task.js
+++ 08_Mongoose_ODM_-_przekroj_dzialania/02_Wymagalnosc_pol/task.js
@@ -1,12 +1,42 @@
 import mongoose from 'mongoose';
 
-import { runAssertions } from './internals/assertions';
+import { runAssertions } from './internals/assertions.js';
 
 (async function () {
   try {
     let appSchema;
 
     // Put your schema here
+    appSchema = new mongoose.Schema({
+      version: {
+        type: String,
+        required: function() {
+          if (this.shouldUpdate) {
+            return true
+          }
+        }
+      },
+      shouldUpdate: {
+        type: Boolean,
+        // required: true
+      },
+      size: {
+        type: Number,
+        // required: true
+      },
+      installationLocation: {
+        type: String,
+        // required: true
+      },
+      lastChecked: {
+        type: Date,
+        required: function() {
+          if (this.shouldUpdate) {
+            return true
+          }
+        }
+      }
+    })
 
     await runAssertions(appSchema);
   } catch (err) {
