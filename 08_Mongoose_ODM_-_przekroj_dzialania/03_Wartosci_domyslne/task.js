1f1c8beccce8c1ba9707e9b3ff4ceebae53831a0
orzeelekk <orzellpawell@gmail.com>
2023-04-05 13:38:38 +0200

    wyk zad 03

--- 08_Mongoose_ODM_-_przekroj_dzialania/03_Wartosci_domyslne/task.js
+++ 08_Mongoose_ODM_-_przekroj_dzialania/03_Wartosci_domyslne/task.js
@@ -1,12 +1,37 @@
 import mongoose from 'mongoose';
 
-import { runAssertions } from './internals/assertions';
+import { runAssertions } from './internals/assertions.js';
 
 (async function () {
   try {
     let transactionSchema;
 
     // Put your schema here
+    transactionSchema = new mongoose.Schema({
+      code: {
+        type: String,
+        required: true
+      },
+      currentVal: {
+        type: Number,
+        required: true
+      },
+      amount: {
+        type: Number,
+        default: 10
+        // required: true
+      },
+      transactionDate: {
+        type: Date,
+        default: new Date().getTime()
+        // required: true
+      },
+      boughtBy: {
+        type: Number,
+        required: true
+      }
+    })
+
 
     await runAssertions(transactionSchema);
   } catch (err) {
