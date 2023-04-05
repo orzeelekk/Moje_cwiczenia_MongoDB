cddfbddd5994f9399455e7c3236722486a2f8b80
orzeelekk <orzellpawell@gmail.com>
2023-04-05 15:16:08 +0200

    wyk zad 05

--- 08_Mongoose_ODM_-_przekroj_dzialania/05_Gettery_i_settery/task.js
+++ 08_Mongoose_ODM_-_przekroj_dzialania/05_Gettery_i_settery/task.js
@@ -1,12 +1,39 @@
 import mongoose from 'mongoose';
 
-import { runAssertions } from './internals/assertions';
+import { runAssertions } from './internals/assertions.js';
 
 (async function () {
   try {
     let stockTransactionSchema;
 
     // Put your schema here
+    stockTransactionSchema = new mongoose.Schema({
+      price: {
+        type: Number,
+        required: true,
+        get: (val) => val && Math.round(val * 100)/100,
+        set: (val) => val && Math.round(val * 100)/100,
+        // get: (val) => roundToTwoDecimals(val),
+        // set: (val) => roundToTwoDecimals(val)
+      },
+      amount: {
+        type: Number,
+        required: true
+      },
+      market: {
+        type: String,
+        required: true
+      },
+      symbol: {
+        type: String,
+        required: true
+      },
+      industry: {
+        type: String,
+        required: true
+      }
+    })
+
 
     await runAssertions(stockTransactionSchema);
   } catch (err) {
