3657a746e4025be26843a2fb4dc891b5032b0f56
orzeelekk <orzellpawell@gmail.com>
2023-04-06 16:22:21 +0200

    wyk zad 04

--- 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/04_Metody_statyczne_schematu/task.js
+++ 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/04_Metody_statyczne_schematu/task.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-import { connectToMongoose } from './internals/connect';
-import { runAssertions } from './internals/assertions';
+import { connectToMongoose } from './internals/connect.js';
+import { runAssertions } from './internals/assertions.js';
 
 (async function () {
   try {
@@ -10,6 +10,38 @@ import { runAssertions } from './internals/assertions';
     let Book;
 
     // Add all of your code below
+    const bookSchema = new mongoose.Schema({
+      author: {type:String, required: true},
+      country: {type:String, required: true},
+      language: {type:String, required: true},
+      link: {type:String, required: true},
+      pages: {type:Number, required: true},
+      title: {type:String, required: true},
+      year: {type:Number, required: true},
+    });
+
+    bookSchema.statics.getBooksByPages = function(min,max) {
+      const query = this.find().where('pages');
+      if (min) {
+        query.gte(min)
+      }
+      if (max) {
+        query.lte(max)
+      }
+      return query.exec()
+    }
+
+    bookSchema.statics.getRandomBookByYear = async function(year) {
+      const query = this.find({year})
+      const books = await query.exec();
+      const randomIndex = Math.floor(Math.random() * books.length);
+      return books.length >= 1 ? books[randomIndex] : books[1];
+    }
+    bookSchema.statics.getBooksByLanguage = function(language) {
+      return this.find({language}).exec()
+    }
+    Book = mongoose.model('Book',bookSchema);
+
 
     await runAssertions(Book);
   } catch (err) {
