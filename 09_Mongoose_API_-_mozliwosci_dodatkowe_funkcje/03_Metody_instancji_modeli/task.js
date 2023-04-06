77ca516d7b5de10da82f58c2346c0b4eec6b6a5f
orzeelekk <orzellpawell@gmail.com>
2023-04-06 14:16:41 +0200

    wyk zad 03

--- 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/03_Metody_instancji_modeli/task.js
+++ 09_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/03_Metody_instancji_modeli/task.js
@@ -10,6 +10,49 @@ import { runAssertions } from './internals/assertions';
     let Order;
 
     // Add all of your code below
+    const productSchema = new mongoose.Schema({
+      name: String,
+      brand: String,
+      available: Number,
+      lastOrderDate: Date,
+      unitPrice: Number,
+      supplierName: String,
+      expirationDate: Date,
+      categories: {
+        type: [String],
+        enum: ['coffee', 'food', 'accessories', 'equipment', 'premium']
+      }
+    });
+
+    const orderSchema = new mongoose.Schema({
+      date: Date,
+      location: String,
+      paidIn: {
+        type: String,
+        enum: ['cash', 'card']
+      },
+      staffId: mongoose.Schema.ObjectId,
+      products: Array,
+      total: {
+        type: Number,
+        min: 1
+      }
+    });
+
+    orderSchema.methods.getRelatedProducts = function() {
+      const productIds = (
+          //mapowanie po naszym zbiorze w populate
+          this.products && this.products.map(product => product.productId)
+      ) || [];
+
+      return Product.find({ _id: { $in: productIds }});
+    };
+
+    Order = mongoose.model('Order', orderSchema);
+
+    const Product = mongoose.model('Product',productSchema);
+
+
 
     await runAssertions(Order);
   } catch (err) {
